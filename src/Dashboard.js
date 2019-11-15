2baa4c8897ca44164c399c5ddd3aee9f95f11b27
jfuller957 <jfuller957@gmail.com>
2019-11-14 17:30:30 -0800

    topic window render's mapped topic(s) and materialize ui's css is functional

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -3,6 +3,9 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -25,7 +28,9 @@ const useStyles = makeStyles(theme => ({
     borderRight: '1px solid grey'
   },
   button: {
-    width: '15%'
+    width: '15%',
+    borderRadius: '15px',
+    background: 'green'
   }
 }));
 
@@ -42,10 +47,21 @@ export default function Dashboard() {
           Topic placeholder
         </Typography>
         <div className={classes.flex}>
-          <div className={classes.topicsWindow}>x</div>
+          <div className={classes.topicsWindow}>
+            <List>
+              {['topic'].map(topic => (
+                <ListItem key={topic} button>
+                  <ListItemText primary={topic} />
+                </ListItem>
+              ))}
+            </List>
+          </div>
           <div className={classes.chatWindow}>x</div>
         </div>
-        <div className={classes.flex}></div>
+        <div className={classes.flex}>
+          <div className={classes.chatBox}>x</div>
+          <div className={classes.button}>x</div>
+        </div>
       </Paper>
     </div>
   );
