226e5faf1d0e47576d5d26542e804935c09784ed
jfuller957 <jfuller957@gmail.com>
2019-11-14 19:33:58 -0800

    initial front end design complete

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -6,6 +6,10 @@ import Typography from '@material-ui/core/Typography';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+// import Avatar from '@material-ui/core/Avatar';
+import Chip from '@material-ui/core/Chip';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -13,11 +17,13 @@ const useStyles = makeStyles(theme => ({
     padding: theme.spacing(3, 2)
   },
   flex: {
-    display: 'flex'
+    display: 'flex',
+    alignItems: 'center'
   },
   chatWindow: {
     width: '70%',
-    height: '300px'
+    height: '300px',
+    padding: '20px'
   },
   chatBox: {
     width: '85%'
@@ -28,20 +34,23 @@ const useStyles = makeStyles(theme => ({
     borderRight: '1px solid grey'
   },
   button: {
-    width: '15%',
-    borderRadius: '15px',
-    background: 'green'
+    margin: theme.spacing(1)
+  },
+  input: {
+    display: 'none'
   }
 }));
 
 export default function Dashboard() {
   const classes = useStyles();
 
+  const [textValue, changeTextValue] = React.useState('');
+
   return (
     <div>
       <Paper className={classes.root}>
         <Typography variant="h4" component="h4">
-          Chat app
+          Community
         </Typography>
         <Typography variant="h5" component="h5">
           Topic placeholder
@@ -56,11 +65,35 @@ export default function Dashboard() {
               ))}
             </List>
           </div>
-          <div className={classes.chatWindow}>x</div>
+          <div className={classes.chatWindow}>
+            {[{ from: 'user', msg: 'hello' }].map((chat, i) => (
+              <div className={classes.flex} key={i}>
+                <Chip label={chat.from} className={classes.chip} />
+                <Typography variant="p" component="p">
+                  &nbsp;:&nbsp;{chat.msg}
+                </Typography>
+              </div>
+            ))}
+          </div>
         </div>
         <div className={classes.flex}>
-          <div className={classes.chatBox}>x</div>
-          <div className={classes.button}>x</div>
+          <div className={classes.chatBox}>
+            <TextField
+              className={classes.chatBox}
+              label="Send a Message"
+              value={textValue}
+              onChange={e => changeTextValue(e.target.value)}
+            />
+          </div>
+          <div className={classes.button}>
+            <Button
+              variant="contained"
+              color="primary"
+              className={classes.button}
+            >
+              Send
+            </Button>
+          </div>
         </div>
       </Paper>
     </div>
